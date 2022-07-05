935e14f62575e661451eeede132b584d2cda45a3
indexsaulomathe <saulo_matheprog@hotmail.com>
2022-07-05 15:59:55 -0400

    create project dojo-plan-vue

---
+++ src/components/CompHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="header-01">
+    <div>
+      <img src="header0001" alt="Logo de Empresa Plan" />
+    </div>
+
+    <div>
+      <span> SAC 0800-607-7779</span>
+      <button>facebook</button>
+      <button>instagram</button>
+      <button>LinkedIn</button>
+      <button>Trabalhe Conosco</button>
+      <button>Area do Cliente @</button>
+    </div>
+
+    <div>
+      <button>Home</button>
+      <button>Empreendimentos</button>
+      <button>ESG</button>
+      <button>Compre um lote</button>
+      <button>Sobre nós</button>
+      <button>Fale com a gente</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-01 {
+  position: absolute;
+  width: 1200px;
+  height: 120px;
+  left: 360px;
+  top: 20px;
+
+  background: #000c3d;
+  opacity: 0.9;
+  border-radius: 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CompHeader from "../components/CompHeader.vue";
+</script>
+
+<template>
+  <main>
+    <CompHeader />
+  </main>
+</template>
