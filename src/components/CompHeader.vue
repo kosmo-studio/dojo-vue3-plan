3c92994b233f9f2fee30ae2d023b1dc2829cad78
indexsaulomathe <saulo_matheprog@hotmail.com>
2022-07-11 00:59:45 -0400

    update in 11/07/2022

--- src/components/CompHeader.vue
+++ src/components/CompHeader.vue
@@ -5,29 +5,62 @@ export default {
 };
 </script>
 <template>
-  <v-app-bar width="100%" height="200">
-    <v-card width="20%"></v-card>
-    <v-card class="d-flex" width="63%" height="180" color="#000C3D">
-      <div class="pa-4 align-self-center">
-        <v-img
-          height="68.48"
-          width="220"
-          aspect-ratio="1"
-          src="src/assets/img/LogoPlan.png"
-        ></v-img>
-      </div>
-      <div class="align-self-end">
-        <div>
-          <p>SAC 0800-607-7779</p>
-          <v-icon></v-icon>
-          <v-icon></v-icon>
-          <v-icon></v-icon>
-          <v-btn></v-btn>
-          <v-btn></v-btn>
+  <v-app-bar width="100%" height="120">
+    <v-row justify="space-around">
+      <div
+        width="63%"
+        max-height="120"
+        class="ma-6 d-flex align-center bg-blue"
+      >
+        <div class="pa-4 pr-15 d-flex align-self-center">
+          <v-img
+            height="100%"
+            width="200"
+            aspect-ratio="1"
+            src="src/assets/img/LogoPlan.png"
+          ></v-img>
         </div>
 
-        <div></div>
+        <v-spacer></v-spacer>
+
+        <div class="ma-4">
+          <v-row justify="end">
+            <span class="ma-5 text-white"> SAC 0800-607-7779</span>
+            <v-icon
+              class="ma-5"
+              icon="fa:fas fa-lock"
+              large
+              color="green darken-2"
+            >
+            </v-icon>
+            <v-icon
+              class="ma-5"
+              large
+              icon="fa:fas fa-lock"
+              color="green darken-2"
+            >
+            </v-icon>
+            <v-icon
+              class="ma-5"
+              large
+              icon="fa:fas fa-lock"
+              color="green darken-2"
+            >
+            </v-icon>
+            <v-btn class="ma-5 bg-green" color="white">Trabalhe Conosco</v-btn>
+            <v-btn class="ma-5 bg-green" color="white">Area do Cliente</v-btn>
+          </v-row>
+          <span></span>
+          <v-row justify="end">
+            <v-btn color="white">Home</v-btn>
+            <v-btn color="white">Empreendimentos</v-btn>
+            <v-btn color="white">ESG</v-btn>
+            <v-btn color="white">Compre um lote</v-btn>
+            <v-btn color="white">Sobre nós</v-btn>
+            <v-btn color="white">Fale com a gente</v-btn>
+          </v-row>
+        </div>
       </div>
-    </v-card>
+    </v-row>
   </v-app-bar>
 </template>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,21 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import { mdi } from "vuetify/iconsets/mdi";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+      mdi,
+    },
+  },
+});
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
